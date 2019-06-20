1ef125368f368a183a532fa429e5b2fb85c87430
SunDoge <384813529@qq.com>
2019-06-20 21:13:58 +0800

    TODO: test load and save

--- test/test_npfile.py
+++ test/test_npfile.py
@@ -1,15 +1,24 @@
 from npfile import NpFile
 import numpy as np
 
+
 def test_dtype():
     dtype = np.float32
 
-    with NpFile('__pycache__', 'w+', dtype=dtype, shape=(1,2)) as fp:
+    with NpFile('__pycache__', 'w+', dtype=dtype, shape=(1, 2)) as fp:
         assert fp.dtype == dtype
 
 
 def test_shape():
-    shape = (1,2)
+    shape = (1, 2)
 
     with NpFile('__pycache__', 'w+', dtype=np.float32, shape=shape) as fp:
         assert fp.shape == shape
+
+
+def test_save():
+    pass
+
+
+def test_load():
+    pass
